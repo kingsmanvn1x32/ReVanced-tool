5615f00d4b328eac9a1ad746e25e96fe0eda1309
kingsmanvn <94727017+Kingsmanvn-Official@users.noreply.github.com>
2022-10-02 23:22:38 +0700

    Update get_buildlog.sh

--- get_buildlog.sh
+++ get_buildlog.sh
@@ -16,13 +16,13 @@ get_prebuilts() {
 	RV_INTEGRATIONS_URL=$(req https://api.github.com/repos/revanced/revanced-integrations/releases/latest - | tr -d ' ' | sed -n 's/.*"browser_download_url":"\(.*apk\)".*/\1/p')
 	RV_INTEGRATIONS_APK=${RV_INTEGRATIONS_URL##*/}
 	RV_INTEGRATIONS_APK="${TEMP_DIR}/${RV_INTEGRATIONS_APK%.apk}-$(cut -d/ -f8 <<<"$RV_INTEGRATIONS_URL").apk"
-	log "Integrations: ${RV_INTEGRATIONS_APK#"$TEMP_DIR/"}"
+	log "\nIntegrations: ${RV_INTEGRATIONS_APK#"$TEMP_DIR/"}"
 
 	RV_PATCHES_URL=$(req https://api.github.com/repos/revanced/revanced-patches/releases/latest - | tr -d ' ' | sed -n 's/.*"browser_download_url":"\(.*jar\)".*/\1/p')
 	RV_PATCHES_JAR="${TEMP_DIR}/${RV_PATCHES_URL##*/}"
 	local rv_patches_filename=${RV_PATCHES_JAR#"$TEMP_DIR/"}
 	local rv_patches_ver=${rv_patches_filename##*'-'}
-	log "Patches: $rv_patches_filename"
+	log "\nPatches: $rv_patches_filename"
 	log "[Patches Changelog](https://github.com/revanced/revanced-patches/releases/tag/v${rv_patches_ver%%'.jar'*})"
 }
 
